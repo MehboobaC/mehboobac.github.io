84b0d324fbf37600ee6429d7e1b1711898542503
Mehbooba <cmehbooba@gmail.com>
2021-10-11 20:12:26 +0530

    corrected some error

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,9 +14,10 @@
 		breakpoints({
 			xlarge:  [ '1281px',  '1680px' ],
 			large:   [ '981px',   '1280px' ],
-			medium:  [ '501px',   '980px'  ],
-			small:   [ null,      '500px'  ]
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
 		});
+
 
 	// Play initial animations on page load.
 		$window.on('load', function() {
@@ -32,3 +33,4 @@
 		});
 
 })(jQuery);
+
