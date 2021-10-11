ed7c65f7f1ce21e3bab2ac688a1b4562387e332a
Mehbooba <cmehbooba@gmail.com>
2021-10-11 19:40:44 +0530

    checking for changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,8 +14,8 @@
 		breakpoints({
 			xlarge:  [ '1281px',  '1680px' ],
 			large:   [ '981px',   '1280px' ],
-			medium:  [ '601px',   '980px'  ],
-			small:   [ null,      '600px'  ]
+			medium:  [ '501px',   '980px'  ],
+			small:   [ null,      '500px'  ]
 		});
 
 	// Play initial animations on page load.
